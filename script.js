0394dcf92a3f334ca34c3b22611fbccbb713de30
Tiago Cazali Magalhães <tiagocazali@gmail.com>
2024-03-22 00:41:49 +0100

    Primeiro Layout Um embaixo do outro

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const adicionarFilme = (produto, preco, quant) => {
     const col1 = linha.insertCell(0)    //Coluna 0 - Nome do Produto
     const col2 = linha.insertCell(1)    //coluna 1 - Prço
     const col3 = linha.insertCell(2)    //coluna 2 - Quantidade
-    const col4 = linha.insertCell(3)    //Botão de Excluir
+    const col4 = linha.insertCell(3)    //Coluna 3 - Botão de Excluir
     
     col1.innerText = produto
     col2.innerText = preco
@@ -38,18 +38,11 @@ tbProdutos.addEventListener("click", (e) => {
     if(e.target.className.includes("exclui")){
         
         const produto = e.target.parentElement.parentElement.children[0].innerText
+        //Lembre-se que o botão está dentro da celula 3 que esta dentro da linha 
         
-        if(confirm("Quer mesmo excluir o filme: " + produto + " ?")){
+        if(confirm("Quer mesmo excluir o Produto: " + produto + " ?")){
             e.target.parentElement.parentElement.remove()
-            localStorage.clear()    //deleta TODO o local Storage
-
-            //agora cria um NOVO LocalStorage só com a lista de filmes que estão na tabela
-            for(let i=1; i<tbProdutos.rows.length; i++){
-                let auxFilme = tbProdutos.rows[i].cells[0].innerText      //coluna 0 - Nome do Filme
-                let auxpreco = tbProdutos.rows[i].cells[1].innerText     //coluna 1 - preco do filme
-
-                gravarLista(auxFilme, auxpreco)
-            }
+        
         }        
     }
 })
